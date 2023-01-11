297660ea9d987b56199b9b10c5ce2ee2895cc05a
Soren Umstot <soren.umstot@gmail.com>
2023-01-11 17:52:24 +0900

    Added PortfolioContainer, Calculate, and Search components in React

--- app/javascript/packs/index.jsx
+++ app/javascript/packs/index.jsx
@@ -7,7 +7,6 @@ import ReactDOM from 'react-dom'
 import PropTypes from 'prop-types'
 import App from '../components/App'
 
-
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
     <App/>,
