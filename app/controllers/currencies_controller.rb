7d37aa1c4fadfc50d1da8e922664421acd82ac8f
Soren Umstot <soren.umstot@gmail.com>
2023-01-11 17:30:56 +0900

    Added some functions to the CurrenciesController and starting work on React front end

--- app/controllers/currencies_controller.rb
+++ app/controllers/currencies_controller.rb
@@ -1,11 +1,26 @@
 class CurrenciesController < ApplicationController
   def index
-
   end
 
   def search
+    @currencies = Currency.where('LOWER(name) LIKE ?', "%#{params[:search].downcase}")
+    render json: { currencies: @currencies }
   end
 
+  #takes in the currency id and the amoutn ownder
   def calculate
+    amount = params[:amount]
+    render json: {
+      currency: currency,
+      current_price: currency.current_price,
+      amount: amount,
+      value: currency.calculate_value(amount)
+    }
+  end
+
+  private
+
+  def currency
+    @currency ||= Currency.find(params[:id])
   end
 end
---
+++ app/javascript/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+class App extends Component {
+  render(){
+    return(
+      <div>Welcome to the App.js file</div>
+    )
+  }
+}
+
+export default App
