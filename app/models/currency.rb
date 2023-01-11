fcae978e371336d1c702640c1179d5db00668f72
Soren Umstot <soren.umstot@gmail.com>
2023-01-11 15:44:56 +0900

    Coinmarketcap api request is working in the currency model.

--- app/models/currency.rb
+++ app/models/currency.rb
@@ -1,2 +1,8 @@
 class Currency < ApplicationRecord
+  def current_price
+    url = "https://pro-api.coinmarketcap.com/v2/cryptocurrency/quotes/latest?slug="
+    request = HTTParty.get(url + self.slug, headers: { "X-CMC_PRO_API_KEY" => Rails.application.credentials.dig(:pro_api_key)}
+  )
+    response = JSON.parse(request.body)
+  end
 end
